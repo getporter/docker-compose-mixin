f3ac8677a97d05937fcd42d226622201f662eb5a
Vaughn Dice <vadice@microsoft.com>
2021-07-06 09:53:45 -0600

    fix(build.go): update installation steps per latest docs
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- pkg/docker-compose/build.go
+++ pkg/docker-compose/build.go
@@ -8,14 +8,9 @@ const dockerComposeVersion = "1.26.0"
 
 // Build installs the docker and docker-compose binaries
 func (m *Mixin) Build() error {
-	dockerfileLines := fmt.Sprintf(`ENV DOCKER_VERSION="19.03.8"
-RUN apt-get update && apt-get install -y python3-pip wget && pip3 install --upgrade pip && \
-  wget https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz && \
-  tar -xvf docker-${DOCKER_VERSION}.tgz && \
-  mv docker/docker /usr/bin/docker && \
-  chmod +x /usr/bin/docker && \
-  rm -rf docker/ docker-${DOCKER_VERSION}.tgz && \
-  pip3 install docker-compose==%s`, dockerComposeVersion)
+	dockerfileLines := fmt.Sprintf(`RUN apt-get update && apt-get install -y curl && \
+curl -L "https://github.com/docker/compose/releases/download/%s/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+chmod +x /usr/local/bin/docker-compose`, dockerComposeVersion)
 
 	fmt.Fprintln(m.Out, dockerfileLines)
 
