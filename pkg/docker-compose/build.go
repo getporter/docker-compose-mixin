2812c70f6d93c31b8ea01a352bfb79f4f074e8fa
Jérémy Audiger <jeremy.audiger@ioterop.com>
2021-07-08 07:41:49 +0200

    [Build] Add the possibility to override the default docker-compose version.
    Signed-off-by: Jérémy Audiger <jeremy.audiger@ioterop.com>

--- pkg/docker-compose/build.go
+++ pkg/docker-compose/build.go
@@ -2,12 +2,46 @@ package dockercompose
 
 import (
 	"fmt"
+
+	"get.porter.sh/porter/pkg/exec/builder"
+	yaml "gopkg.in/yaml.v2"
 )
 
-const dockerComposeVersion = "1.26.0"
+const dockerComposeDefaultVersion = "1.29.2"
+
+// BuildInput represents stdin passed to the mixin for the build command.
+type BuildInput struct {
+	Config MixinConfig
+}
+
+// MixinConfig represents configuration that can be set on the docker-compose mixin in porter.yaml
+// mixins:
+// - docker-compose:
+//     clientVersion: 1.29.2
+
+type MixinConfig struct {
+	ClientVersion string `yaml:"clientVersion,omitempty"`
+}
 
 // Build installs the docker and docker-compose binaries
 func (m *Mixin) Build() error {
+	// Create new Builder.
+	var input BuildInput
+	err := builder.LoadAction(m.Context, "", func(contents []byte) (interface{}, error) {
+		err := yaml.Unmarshal(contents, &input)
+		return &input, err
+	})
+	if err != nil {
+		return err
+	}
+
+	var dockerComposeVersion string
+	if input.Config.ClientVersion != "" {
+		dockerComposeVersion = input.Config.ClientVersion
+	} else {
+		dockerComposeVersion = dockerComposeDefaultVersion
+	}
+
 	dockerfileLines := fmt.Sprintf(`RUN apt-get update && apt-get install -y curl && \
 curl -L "https://github.com/docker/compose/releases/download/%s/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose`, dockerComposeVersion)
--- pkg/docker-compose/build_test.go
+++ pkg/docker-compose/build_test.go
@@ -1,6 +1,8 @@
 package dockercompose
 
 import (
+	"bytes"
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,7 +11,7 @@ import (
 
 func TestMixin_Build(t *testing.T) {
 	const buildOutput = `RUN apt-get update && apt-get install -y curl && \
-curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose && \
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose
 `
 
@@ -23,5 +25,33 @@ chmod +x /usr/local/bin/docker-compose
 		wantOutput := buildOutput
 		gotOutput := m.TestContext.GetOutput()
 		assert.Equal(t, wantOutput, gotOutput)
+	})
+
+	t.Run("build with a defined docker-compose client version", func(t *testing.T) {
+		b, err := ioutil.ReadFile("testdata/build-input-with-version.yaml")
+		require.NoError(t, err)
+
+		m := NewTestMixin(t)
+		m.Debug = false
+		m.In = bytes.NewReader(b)
+
+		err = m.Build()
+		require.NoError(t, err, "build failed")
+
+		wantOutput := buildOutput
+		gotOutput := m.TestContext.GetOutput()
+		assert.Equal(t, wantOutput, gotOutput)
+	})
+
+	t.Run("build with invalid config", func(t *testing.T) {
+		b, err := ioutil.ReadFile("testdata/build-input-with-invalid-config.yaml")
+		require.NoError(t, err)
+
+		m := NewTestMixin(t)
+		m.Debug = false
+		m.In = bytes.NewReader(b)
+
+		err = m.Build()
+		require.Error(t, err, "build failed")
 	})
 }
