160f0725243196f7a75d24e5815179a963c04480
Vaughn Dice <vadice@microsoft.com>
2021-07-15 10:39:55 -0600

    address review feedback
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- pkg/docker-compose/build.go
+++ pkg/docker-compose/build.go
@@ -9,7 +9,7 @@ const dockerComposeVersion = "1.26.0"
 // Build installs the docker and docker-compose binaries
 func (m *Mixin) Build() error {
 	dockerfileLines := fmt.Sprintf(`RUN apt-get update && apt-get install -y curl && \
-curl -L "https://github.com/docker/compose/releases/download/%s/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+curl -L "https://github.com/docker/compose/releases/download/%s/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose`, dockerComposeVersion)
 
 	fmt.Fprintln(m.Out, dockerfileLines)
--- pkg/docker-compose/build_test.go
+++ pkg/docker-compose/build_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestMixin_Build(t *testing.T) {
 	const buildOutput = `RUN apt-get update && apt-get install -y curl && \
-curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose
 `
 
